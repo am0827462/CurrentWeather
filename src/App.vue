e928d0959eea7fc658a481f3e0ccb2083e6481c9
Angela McLaughlin <amclaughlin@SEA.CORP.EXPECN.com>
2020-12-09 18:33:57 -0600

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,124 @@
+<!--
+    Name: Angela McLaughlin
+    Class Section: CIS-131-W01-Fall 2020
+    Date: 12/5/2020
+-->
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <Header theTitle="Current Weather" paragraph="Enter a valid five digit US Zip Code, select Celsius or Fahrenheit, and click 'Current Weather' button to see the current weather for the location."/>
+    <form>
+      <div class="form-row align-items-center">
+          <div class="col-auto">
+              <label class="sr-only" for="inlineFormInput">Zip Code</label>
+              <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Zip Code" v-model="zip">
+          </div>
+          <div class="col-auto">
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="degrees" id="F" value="F" v-model="degrees" v-on:change="updateTemp()">
+                <label class="form-check-label" for="F">
+                    Fahrenheit
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="degrees" id="C" value="C" v-model="degrees" v-on:change="updateTemp()">
+                <label class="form-check-label" for="C">
+                    Celsius
+                </label>
+            </div>
+          </div>
+          <div class="col-auto">
+              <button type="button" class="btn btn-primary mb-2" v-on:click="validateZip()">Current Weather</button>
+          </div>
+          </div>
+            <!-- Error Message -->
+            <div v-if="error !== null">
+                <h5 style="color:red">{{error}}</h5>
+            </div>
+      </form>
+    <div v-if="error === null && temp !== null">
+    <Card v-bind:cityprop="city" v-bind:degreesprop="degrees" v-bind:tempprop="temp" v-bind:currentprop="current" v-bind:iconprop="icon" v-bind:descprop="description"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Card from './components/Card.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Card
+  },
+  data(){
+    return{
+      tempKelvin: null,
+      city: "",
+      current: "",
+      description:"",
+      icon:"",
+      zip: "",
+      degrees: "F",
+      temp: null,
+      error: null,
+      regex: /\b\d{5}\b/g
+    }
+  },
+  methods: {
+    //Validates Zip code before API call
+    validateZip(){
+        if(this.zip === ""){
+            this.error = "Please enter a zip code."
+        }
+        else if (this.regex.test(this.zip)===false){
+            this.error = "Zip code must be five digits."
+        }
+        else{
+        this.error = null
+        this.apiCall()}
+    },
+
+    //Toggle from F to C changes already displayed temp and called during inital api
+    updateTemp(){
+        //Convert from Kelvin to selection on radio button
+        if(this.error === null && this.tempKelvin !== null){
+            if(this.degrees==="F"){
+                this.temp = ((this.tempKelvin - 273.15) * (9/5) + 32).toFixed(0)
+            }
+            else{
+                this.temp = (this.tempKelvin - 273.15).toFixed(0)
+            }
+        }
+    },
+    
+    apiCall(){
+    this.temp = null
+    //Use Axios to grab weather using zip entered in form
+    axios.get("http://api.openweathermap.org/data/2.5/weather?zip=" + this.zip +",us&appid=c234a81ae7d1d5eba19ea29fb50f695e")
+    .then((response) => {
+        //Place weather data into variables
+        this.tempKelvin = response.data.main.temp
+        this.city = response.data.name
+        this.current = response.data.weather[0].main
+        this.description = response.data.weather[0].description
+        this.icon = response.data.weather[0].icon
+        this.updateTemp() //Convert temp
+    //catches error with api call returns the message that the zip is invalid
+    }).catch((err) => {
+        console.log(err)
+        this.error = "Zip code is not valid."
+    })
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-image: url('./assets/background.jpg');
+  background-size: cover;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<!--
+    Name: Angela McLaughlin
+    Class Section: CIS-131-W01-Fall 2020
+    Date: 12/5/2020
+-->
+
+<template>
+    <div class="card mb-3 bg-secondary text-white" style="max-width: 540px;">
+        <div class="row no-gutters">
+        <div class="col-md-4">
+        <img v-bind:src="'http://openweathermap.org/img/wn/' + iconprop + '@2x.png'" class="card-img-top" alt="icon">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">{{cityprop}} Current Conditions</h5>
+            <p class="card-text">Temperature: {{tempprop}}° {{degreesprop}}</p>
+            <p class="card-text">Description: {{currentprop}}</p>
+            <p class="card-text">Detail: {{descprop}}</p>
+        </div>   
+    </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Card",
+    props:['cityprop','tempprop','degreesprop','currentprop','iconprop','descprop']
+}
+</script>
